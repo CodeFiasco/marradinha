0b21ef62164129ed47da5458f05a60894a265787
rodrigo.duarte <rodrigo.duarte@academiadecodigo.org>
2018-06-04 11:04:18 +0000

    Server and clientHelper working

---
+++ src/org/academiadecodigo/bootcamp/marralhinha/client/ClientHelper.java
@@ -0,0 +1,50 @@
+package org.academiadecodigo.bootcamp.marralhinha.client;
+
+import org.academiadecodigo.bootcamp.marralhinha.server.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientHelper implements Runnable {
+
+    private Server server;
+    private PrintWriter out;
+    private BufferedReader in;
+    private Socket clientSocket;
+
+
+    public ClientHelper(Socket clientSocket) {
+
+        try {
+
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+
+        while (clientSocket.isBound()) {
+
+            System.out.println("msg: ");
+
+            String message = null;
+
+            try {
+                message = in.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            out.println(message);
+        }
+
+    }
+}
--- src/org/academiadecodigo/bootcamp/marralhinha/server/Server.java
+++ src/org/academiadecodigo/bootcamp/marralhinha/server/Server.java
@@ -1,4 +1,52 @@
 package org.academiadecodigo.bootcamp.marralhinha.server;
 
+import org.academiadecodigo.bootcamp.marralhinha.client.ClientHelper;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
+
+    private final static int PORT_NUMBER = 9000;
+    private List<ClientHelper> clientList = new ArrayList<>(4);
+    private ClientHelper clientHelper;
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
+
+    private void start() {
+
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(9090);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while (true) {
+
+
+            Socket clientSocket = null;
+            try {
+                clientSocket = serverSocket.accept();
+
+                System.out.println("Server is now connected and waiting for requests at port " + PORT_NUMBER);
+                ClientHelper clientHelper = new ClientHelper(clientSocket);
+                clientList.add(clientHelper);
+
+            } catch (IOException e) {
+                System.err.println("Missing port number");
+            }
+
+        }
+
+    }
+
+
 }
